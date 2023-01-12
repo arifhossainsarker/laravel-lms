85770589a3e6d1617915dedd904dcdff33931b1a
Arif Hossain <arifhossain393@gmail.com>
2023-01-12 16:17:30 +0600

    user function added

--- api/database/migrations/2023_01_11_065839_create_courses_table.php
+++ api/database/migrations/2023_01_11_065839_create_courses_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('description');
             $table->string('image');
             $table->integer('user_id');
+            $table->timestamps();
         });
     }
 
--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Course;
+use App\Models\Currcilam;
+use App\Models\Lead;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
@@ -25,23 +28,63 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $user = new User();
-        $user->name = 'Super Admin';
-        $user->email = 'super@admin.com';
-        $user->password = bcrypt('password');
-        $user->save();
+        // $user = new User();
+        // $user->name = 'Super Admin';
+        // $user->email = 'super@admin.com';
+        // $user->password = bcrypt('password');
+        // $user->save();
 
 
+        // $role = Role::create([
+        //     'name' => 'Super Admin'
+        // ]);
+
+        // $permission = Permission::create([
+        //     'name' => 'create-admin'
+        // ]);
+
+        // $role->givePermissionTo($permission);
+        // $permission->assignRole($role);
+
+        // $user->assignRole($role);
+
+        $this->create_user_with_role('super-admin', 'Super Admin', 'super@admin.com');
+        $this->create_user_with_role('admin', 'admin', 'admin@admin.com');
+        $teacher = $this->create_user_with_role('teacher', 'teacher', 'teacher@admin.com');
+
+        // create lead
+        $lead = Lead::factory()->count(100)->create();
+
+        $course = Course::create([
+            'name' => 'Laravel',
+            'description' => 'As an example, let modify the default DatabaseSeeder class and add a database insert statement to the run method',
+            'image' => 'https://cdn.worldvectorlogo.com/logos/laravel-2.svg',
+            'user_id' => 3,
+        ]);
+
+        $currcilam = Currcilam::factory()->count(10)->create();
+    }
+
+    private function create_user_with_role($type, $name, $email)
+    {
         $role = Role::create([
-            'name' => 'Super Admin'
+            'name' => $type
         ]);
 
-        $permission = Permission::create([
-            'name' => 'create-admin'
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt('password')
         ]);
 
-        $role->givePermissionTo($permission);
-        $permission->assignRole($role);
+        if ($type == 'super-admin') {
+            $permission = Permission::create([
+                'name' => 'create-admin'
+            ]);
+
+            $role->givePermissionTo($permission);
+            $permission->assignRole($role);
+        }
 
         $user->assignRole($role);
     }
